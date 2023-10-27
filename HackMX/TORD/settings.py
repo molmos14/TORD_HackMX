2fd489da4c70ceddaf62782b68d7f7f22fb8d361
molmos14 <A01750748@tec.mx>
2023-10-27 17:29:42 -0600

    test conection db

--- HackMX/TORD/settings.py
+++ HackMX/TORD/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,10 +75,22 @@ WSGI_APPLICATION = 'TORD.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'netlfix',
+        'USER': 'tord',
+        'PASSWORD': 'PaMax/150814',
+        'HOST': 'localhost',  # O la dirección IP si tu base de datos está en un servidor remoto
+        'PORT': '3306',  # El puerto puede variar dependiendo de tu configuración
     }
 }
 
