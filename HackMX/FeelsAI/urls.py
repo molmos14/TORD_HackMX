4998542571bd3e330e58ec703b627a14461232a2
César Antonio Espinosa Madrid <A01799815@tec.mx>
2023-10-27 16:59:32 -0600

    Time test

--- HackMX/FeelsAI/urls.py
+++ HackMX/FeelsAI/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from . import views
 urlpatterns = [
-path('', include('appname.urls')),
+path('',views.current_datetime, name="current_datetime"),
+path('admin/', admin.site.urls),
 ]
