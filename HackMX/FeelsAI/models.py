69cc0cc4fc2029f1a580ccd13a3ddccf9756136a
César Antonio Espinosa Madrid <A01799815@tec.mx>
2023-10-27 20:17:42 -0600

    .

--- HackMX/FeelsAI/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Movie(models.Model):
-    title = models.CharField(max_length=200)
-    release_year = models.IntegerField()
-
-    def __str__(self):
-        return self.title
